b84609c52b4483eb93442c7dfb88ba04f98af92a
seriousbee <tczernuszenko@gmail.com>
2015-11-01 07:42:51 +0100

    added .gitingnore

--- app/src/main/java/com/ulluna/braillemessaging/MainActivity.java
+++ app/src/main/java/com/ulluna/braillemessaging/MainActivity.java
@@ -24,8 +24,8 @@ public class MainActivity extends Activity {
     public MediaPlayer mp;
     GestureDetectorCompat mDetector;
     public TextToSpeech tts;
-    String[] litery = {"", "A", "", "B", "", "K", "", "L", "", "C", "I", "F", "", "M", "S", "P", "", "E", "", "H", "", "O", "", "R", "", "D", "J", "G", "", "N", "T", "Q", "", "Ą", "", "Ł", "", "U", "", "V", "", "Ć", "Ś", "", "Ó", "X", "Ź", "Ż", "", "Ę", "", "", "", "Z", "", "", "", "Ń", "W", "", "", "Y", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
-    String[] liczby = {"", "1", "", "2", "", "K", "", "L", "", "3", "9", "6", "", "M", "S", "P", "", "5", "", "8", "", "O", "", "R", "", "4", "0", "7", "", "N", "T", "Q", "", "Ą", "", "Ł", "", "U", "", "V", "", "Ć", "Ś", "", "Ó", "X", "Ź", "Ż", "", "Ę", "", "", "", "Z", "", "", "", "Ń", "W", "", "", "Y", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+    String[] letters = {"", "A", "", "B", "", "K", "", "L", "", "C", "I", "F", "", "M", "S", "P", "", "E", "", "H", "", "O", "", "R", "", "D", "J", "G", "", "N", "T", "Q", "", "Ą", "", "Ł", "", "U", "", "V", "", "Ć", "Ś", "", "Ó", "X", "Ź", "Ż", "", "Ę", "", "", "", "Z", "", "", "", "Ń", "W", "", "", "Y", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+    String[] numbers = {"", "1", "", "2", "", "", "", "", "", "3", "9", "6", "", "", "", "", "", "5", "", "8", "", "", "", "", "", "4", "0", "7", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
     int[] bests = new int[70];
     int screenWidth;
     int screenHeight;
@@ -52,29 +52,34 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         Log.d(DEBUG_TAG, "I made it");
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+
+        //hide the status bar (time, notifications etc)
         ActionBar bar = getActionBar();
-        bar.hide();
+        if (bar!=null)
+            bar.hide();
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);// ukrycie status bar (powiadomienia, zegar itp)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        //Get size of the screen
         WindowManager w = getWindowManager();
-
         Point size = new Point();
         w.getDefaultDisplay().getSize(size);
         screenWidth = size.x;
         screenHeight = size.y;
 
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());//rejestracja listnera gestów
+        //register the GestureDetector
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
         mp = MediaPlayer.create(getApplicationContext(), R.raw.startbeep);
         mp.start();
 
     }
 
+
+    //Determine what happens when the screen in pressed
     @Override
-    public boolean onTouchEvent(MotionEvent event){ //co się ma stać po wciśnięciu w ekran
+    public boolean onTouchEvent(MotionEvent event){
         this.mDetector.onTouchEvent(event);
         pressedValue=0;
         final int actionPeformed = event.getAction();
@@ -83,74 +88,76 @@ public class MainActivity extends Activity {
         textview2 = (TextView)findViewById(R.id.textView);
         textview3 = (TextView)findViewById(R.id.textView2);
 
-
-        Log.d(DEBUG_TAG, event.getPointerCount()+"");
-
+        //Log and Save to an array the registered coordinates of pressed points
         printSamples(event);
 
-        for(int i=5; i>=0; i--){ //sprawdzenie w które części ekranu/kropki wciska użytkownik i zsumowanie wartości wciśniętej przez użytkownika. Każdy obszar odpowiada kolejnemu rzędowi wielkości z systemu binarnego. Pozostały obszar(środek ekranu to 64)
-            for (int k=0; k<event.getPointerCount(); k++){
-                if(pointsArr[k][0]>=screenWidth-(screenHeight/3)*1.5 && pointsArr[k][1]>=screenHeight-screenHeight/3){
-                    pressedValue+=32;
-                }
-                else if(pointsArr[k][0]>=screenWidth-(screenHeight/3)*1.5 && pointsArr[k][1]<screenHeight-screenHeight/3 && pointsArr[k][1]>screenHeight/3){
-                    pressedValue+=16;
-                }
-                else if(pointsArr[k][0]>=screenWidth-(screenHeight/3)*1.5 && pointsArr[k][1]<=screenHeight/3){
-                    pressedValue+=8;
-                }
-                else if(pointsArr[k][0]<=(screenHeight/3)*1.5 && pointsArr[k][1]>=screenHeight-screenHeight/3){
-                    pressedValue+=4;
-                }
-                else if(pointsArr[k][0]<=(screenHeight/3+50)*1.5 && pointsArr[k][1]<screenHeight-screenHeight/3 && pointsArr[k][1]>screenHeight/3){
-                    pressedValue+=2;
-                }
-                else if(pointsArr[k][0]<=(screenHeight/3)*1.5 && pointsArr[k][1]<=screenHeight/3){
-                    pressedValue+=1;
-                }
-                else{
-                    pressedValue+=64;
-                }
+        //Determine which areas the user is pressing. Every area has an attributed value (consecutive multiples of 2). This allows the sum of any combination of pressed areas to always be unique. 64 represents the middle part of the screen (the app is not concerned about this area)
+
+        for (int k=0; k<event.getPointerCount(); k++){
+            if(pointsArr[k][0]>=screenWidth-(screenHeight/3)*1.5 && pointsArr[k][1]>=screenHeight-screenHeight/3){
+                pressedValue+=32;
             }
-
+            else if(pointsArr[k][0]>=screenWidth-(screenHeight/3)*1.5 && pointsArr[k][1]<screenHeight-screenHeight/3 && pointsArr[k][1]>screenHeight/3){
+                pressedValue+=16;
+            }
+            else if(pointsArr[k][0]>=screenWidth-(screenHeight/3)*1.5 && pointsArr[k][1]<=screenHeight/3){
+                pressedValue+=8;
+            }
+            else if(pointsArr[k][0]<=(screenHeight/3)*1.5 && pointsArr[k][1]>=screenHeight-screenHeight/3){
+                pressedValue+=4;
+            }
+            else if(pointsArr[k][0]<=(screenHeight/3+50)*1.5 && pointsArr[k][1]<screenHeight-screenHeight/3 && pointsArr[k][1]>screenHeight/3){
+                pressedValue+=2;
+            }
+            else if(pointsArr[k][0]<=(screenHeight/3)*1.5 && pointsArr[k][1]<=screenHeight/3){
+                pressedValue+=1;
+            }
+            else{
+                pressedValue+=64;
+            }
         }
-        pressedValue/=6;//z jakiegoś powodu wynik jest 6 razy większy od oczekiwanego
 
-        textview.setText(String.valueOf(pressedValue)); //wyświetlenie wyniku
+        //display the pressed value
+        textview.setText(String.valueOf(pressedValue));
+
+        //vibrate the phone to give feeedback
         Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
         if (pressedValue>0 && pressedValue<65){
             v.vibrate(50);
-            bests[pressedValue]++; //zliczanie, którą kombinację użytkownik wciskał najdłużej (działa dopuki użytkownik wciska w ekran)
+            bests[pressedValue]++; //count how long each combination was pressed (if the user moves his fingers it will still be registered and we have to find the longest - most probable option)
         }
 
         switch(actionPeformed){
+            //what happens when the user does not press any more
             case MotionEvent.ACTION_UP:{
-
                 pressedValue=0;
                 textview.setText(String.valueOf(pressedValue));
                 int maxIndex = 0;
-                for (int i = 1; i < bests.length; i++){//szukanie która wartość była najdłużej wciskana
+                for (int i = 1; i < bests.length; i++){//find which value was pressed the longest amount of time
                     int newnumber = bests[i];
                     if ((newnumber > bests[maxIndex])){
                         maxIndex = i;
                     }
                 }
-                for (int i =0; i< bests.length; i++){//resetowanie tablicy
+                for (int i =0; i< bests.length; i++){//reset the array for the next measurment
                     bests[i]=0;
                 }
-                if (maxIndex==64){//jeżeli użytkownik nie wcisnął w żaden obszar/kropkę
+                if (maxIndex==64){//if the user did not press any relevant area
                     break;
                 }
-                if (maxIndex==60){//znak liczby
+                if (maxIndex==60){//special sign indicating user wants to put next in a number and not a character
                     isNumber=true;
                     textview2.setText("Number sign");
                     tts.speak("Numbers are on", TextToSpeech.QUEUE_FLUSH, null);
                     break;
                 }
-                if (!isNumber&& !phoneNumber){//ustawienie odpowiedniej litery/liczby
-                    textview2.setText(litery[maxIndex]);
-                }else {
-                    textview2.setText(liczby[maxIndex]);
+                if (!isNumber&& !phoneNumber){//set the right
+                    textview2.setText(letters[maxIndex]);
+                }
+
+                else {
+                    textview2.setText(numbers[maxIndex]);
                     if (phoneNumber){//dodanie cyfry do numeru telefonu
                         numberPhone += textview2.getText().toString();
                         textview3.setText(numberPhone);
@@ -189,7 +196,7 @@ public class MainActivity extends Activity {
 
     public class MyGestureListener extends GestureDetector.SimpleOnGestureListener {//tworzenie listnera gestów
 
-        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MIN_DISTANCE = 200;
         private static final int SWIPE_THRESHOLD_VELOCITY = 120;
         TextToSpeech tts2=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -287,7 +294,7 @@ public class MainActivity extends Activity {
                     Math.abs(velocityY) > SWIPE_THRESHOLD_VELOCITY) {//czytanie, co do tej pory zawiera wiadomość lub numer telefonu
                 //From Top to Bottom
                 if (!phoneNumber){
-                    tts2.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+                    tts2.speak(message.toLowerCase(), TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else{
                     tts2.speak(numberToRead(numberPhone), TextToSpeech.QUEUE_FLUSH, null);
